6e6158d31f406ba4709d9449b0ecc2c2fb5d65a7
duke mai <duc.mai@niteco.se>
2018-04-06 00:27:40 +0200

    update portfolio

--- www/gatsby-config.js
+++ www/gatsby-config.js
@@ -2,5 +2,19 @@ module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
   },
-  plugins: [`gatsby-plugin-react-helmet`],
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-117035385-1",
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+      },
+    },
+  ],
 }
